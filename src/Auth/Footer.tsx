c4cba18c72b6991e2fd016c6e6af65257555c62f
Jesse Pence <jessepence@gmail.com>
2023-03-08 11:57:11 -0800

    jesus h christ

--- src/Auth/Footer.tsx
+++ src/Auth/Footer.tsx
@@ -7,9 +7,9 @@ import { useSession } from "next-auth/react"
 function Footer() {
   const { status } = useSession()
 
-  if (status === "loading") return null
+  if (status === "loading") return <h3>Loading Auth Footer...</h3>
 
-  if (status === "unauthenticated") return null
+  if (status === "unauthenticated") return <h3>You are not signed in.</h3>
 
   return <button onClick={() => signOut()}>Sign Out</button>
 }
